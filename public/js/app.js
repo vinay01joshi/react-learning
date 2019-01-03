2ad82eff78fef36d501475e72706d66ae67860f3
Vinay Joshi <vinay01joshi@gmail.com>
2019-01-03 14:20:05 +0530

    Exporting React code to ES6 React Class component

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,6 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var createReactClass = require('create-react-class');
-//console.log(React);
-
-var Hello = createReactClass({
-    render: function () {
-        //return React.createElement("h3",null ,"Hello WebPack!");
-        return <h3>Hello JSX ?</h3>
-    }
-});
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './components/hello';
 
 
 ReactDOM.render(<Hello/>,document.getElementById('react'));
---
+++ public/js/components/hello.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Hello extends React.Component {
+
+    render() {
+        return <h3> Hello React Component with Cool Features ! </h3>
+    }
+
+}
