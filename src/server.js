b9e8f5a89e48fa6da2445f82f8750cddbf413b93
Vinay Joshi <vinay01joshi@gmail.com>
2019-01-12 20:36:26 +0530

    Fixing Babel Issue

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 
+import { MongoClient } from 'mongodb';
+
 let app = express();
 
 //app.get('/',(req,res) => res.send('Hello NodeMon!'));
@@ -7,3 +9,17 @@ let app = express();
 app.use(express.static('public'));
 
 app.listen(3000);
+
+console.log(process.env.MONGO_URL);
+
+MongoClient.connect(process.env.MONGO_URL,(err,client) => {
+    if(err) throw err ;
+    
+    let db = client.db('reactlearning');
+
+    db.collection("links").find({}).toArray((err,links)=>{
+        if(err) throw err ;
+
+        console.log(links);
+    })
+});
